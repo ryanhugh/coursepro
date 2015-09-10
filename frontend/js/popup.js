aa709f39ef2a8a77681f3198d718b2ad14c1cea8
Ryan <rysquash+bitbucket@gmail.com>
2015-09-09 22:03:46 -0400

    sorting, i think

--- frontend/js/popup.js
+++ frontend/js/popup.js
@@ -140,6 +140,46 @@ Popup.prototype.groupSectionTimes = function(sections) {
 		section.profs = section.profs.join(', ');
 		section.locations = section.locations.join(', ');
 	}.bind(this))
+	
+	
+	//sort sections by first grouped time start time
+	sections.sort(function(a,b){
+	  if (!a.meetings && !b.meetings) {
+	    return 0;
+	  }
+	  if (!a.meetings) {
+	    return 1;
+	  }
+	  if (!b.meetings) {
+	    return -1;
+	  }
+	  
+	  if (a.meetings.groupedTimes.length===0) {
+	    return 1;
+	  }
+	  if (b.meetings.groupedTimes.length===0) {
+	    return -1;
+	  }
+	  if (a.meetings.groupedTimes[0].times.length===0) {
+	    return 1;
+	  }
+	  if (b.meetings.groupedTimes[0].times.length===0) {
+	    return -1;
+	  }
+	  if (a.meetings.groupedTimes[0].times[0].startTime>b.meetings.groupedTimes[0].times[0].startTime) {
+	    return 1;
+	  }
+	  else if (a.meetings.groupedTimes[0].times[0].startTime<b.meetings.groupedTimes[0].times[0].startTime) {
+	    return -1;
+	  }
+	  else {
+	    return 0;
+	  }
+	  
+	}.bind(this))
+	
+	
+	
 return sections;
 }
 Popup.prototype.createViewOnUrl = function(tree,url) {
@@ -247,7 +287,7 @@ Popup.prototype.expandPanel = function(tree) {
 
 			//add credits to the left
 			if (tree.minCredits!==undefined || tree.maxCredits!==undefined) {
-				newBodyText.push('<div style="text-align:left;display: inline-block; position: absolute;max-width: 50%;">'+this.createCreditsHTML(tree)+'</div>')				
+				newBodyText.push('<div style="text-align:left;display: inline-block; position: absolute;max-width: 50%;">'+this.createCreditsHTML(tree)+'</div>')
 			}
 
 			var classURL ;
@@ -343,7 +383,7 @@ Popup.prototype.expandPanel = function(tree) {
 			tree.panel.style.top = Math.max(tree.y - tree.panel.offsetHeight/2,$('.navbar')[0].offsetHeight+25 ) + 'px'
 
 
-			//shadows are cool 
+			//shadows are cool
 			tree.panel.style.boxShadow = 'gray 0px 0px 9px'
 
 
