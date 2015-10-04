2c179edc4a10df845772058705f8d972adde77fe
rysquash <rysquash@gmail.com>
2015-10-04 01:13:07 -0400

    updated logging sh files

--- tail.py
+++ tail.py
@@ -1,23 +1,29 @@
 import json
 import re
+import datetime
 
 while 1:
-		try:
-			a= raw_input()
-		except:
-			exit()
-		if not a:
-				exit()
-		try:
-				b= json.loads(a)
-		except:
-				print a
-				continue
-		if 'userId' in b['body']:
-				b['body']['userId']=re.sub('[\W_]', '',b['body']['userId'])[0:5]
+                try:
+                        a= raw_input()
+                except:
+                        exit()
+                if not a:
+                                exit()
+                try:
+                                b= json.loads(a)
+                except:
+                                print a
+                                continue
 
-		ref = ''
-		if 'referer' in b and 'coursepro.io' not in b['referer']:
-				print b['referer'],
-		print b['ip'],' ',b['url'],b['body']
+                if 'searchTerm' in b:
+                        print 'Search for','"'+b['searchTerm']+'"',':',b['count']
+                        continue
 
+
+                if 'userId' in b['body']:
+                                b['body']['userId']=re.sub(r'[\W_]', '',b['body']['userId'])[0:5]
+
+                ref = ''
+                if 'referer' in b and 'coursepro.io' not in b['referer']:
+                                print b['referer'],
+                print b['ip'],datetime.datetime.fromtimestamp(int(b['time'])/1000).ctime(),' ',b['url'],b['body']
