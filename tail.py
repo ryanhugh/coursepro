bfba4d979ab27475391606a6a3f13c3024c44e95
Ryan <rysquash+bitbucket@gmail.com>
2015-10-17 14:45:52 -0400

    tail.py shows today and yesterday

--- tail.py
+++ tail.py
@@ -1,34 +1,65 @@
 import json
 import re
-import datetime
+# import datetime
+from datetime import datetime, time,timedelta
+import exceptions
 
 while 1:
-                try:
-                        a= raw_input()
-                except:
-                        exit()
-                if not a:
-                                exit()
-                try:
-                                b= json.loads(a)
-                except:
-                                print a
-                                continue
+	try:
+		a= raw_input()
+	except exceptions.EOFError:
+		exit()
+	except Exception as e:
+		print e.type
+		exit()
+	if not a:
+		exit()
+	try:
+		b= json.loads(a)
+	except:
+		print a
+		continue
 
-                if 'searchTerm' in b:
-                        print 'Search for','"'+b['searchTerm']+'"',':',b['count']
-                        continue
+	if 'searchTerm' in b:
+		print 'Search for','"'+b['searchTerm']+'"',':',b['count']
+		continue
+
+	#shorten the userId
+	if 'userId' in b['body']:
+		b['body']['userId']=re.sub(r'[\W_]', '',b['body']['userId'])[0:5]
 
 
-                if 'userId' in b['body']:
-                                b['body']['userId']=re.sub(r'[\W_]', '',b['body']['userId'])[0:5]
+    #print the referrer, if it is not coursepro.io
+	if 'referer' in b and 'coursepro.io' not in b['referer']:
+		print b['referer'],
+        
+        
+	print b['ip'],
+	
+        
+	utcnow = datetime.utcnow()
+	midnight_utc = datetime.combine(utcnow.date(), time(0))
+	
+	reqTime = datetime.fromtimestamp(int(b['time'])/1000)
+	if reqTime>midnight_utc :
+		print 'Today',
+	elif reqTime>(midnight_utc- timedelta(days=1)):
+		print 'Yesterday',
+	else :
+		print reqTime.strftime('%b %d %I:%M:%S %p'),
+		
+	
+	
+	
+	print reqTime.strftime('%I:%M:%S %p'),
+		
+	print ' ',b['url'],
+    	
+        
+       
+        
 
-                ref = ''
-                if 'referer' in b and 'coursepro.io' not in b['referer']:
-                                print b['referer'],
-                print b['ip'],datetime.datetime.fromtimestamp(int(b['time'])/1000).strftime('%b %d %I:%M:%S %p'),' ',b['url'],
-
-		if b['body']!={}:
-			print b['body']
-		else:
-			print
+	if b['body']!={}:
+		print b['body']
+	else:
+		print
