06b3b3e5cf2b45e9715e46e7604a24c4b35969b0
Ryan <rysquash+bitbucket@gmail.com>
2015-11-12 12:38:26 -0500

    clicking home button multiple times will not add to history

--- frontend/js/homepage.js
+++ frontend/js/homepage.js
@@ -1,16 +1,5 @@
+'use strict';
 
-//google analytics
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
-  ga('create', 'UA-67419497-1', 'auto');
-  ga('send', 'pageview');
-  
-  
-  
-  
 function Homepage () {
 	
 	
@@ -27,7 +16,28 @@ Homepage.prototype.removeHomepage = function() {
 };
 
 
+Homepage.prototype.isOnHomepage = function() {
+	
+	// if the element has a parent, it is in the container, if not it needs to be added
+	if (this.homepageElement.parentElement) {
+		return true
+	}
+	else {
+		return false;
+	}
+}
+
+
+
 Homepage.prototype.clickHomeButton = function() {
+	
+	// if the element has a parent, it is in the container, if not it needs to be added
+	if (this.isOnHomepage()) {
+		return;
+	}
+	
+	
+	
 	history.pushState(null, null, "#");// only do this if icon clicked, not if back used to show
 	this.show();
 };
@@ -35,19 +45,19 @@ Homepage.prototype.clickHomeButton = function() {
 
 Homepage.prototype.show = function () {
 	
-	if (this.isOnHomepage && 0) {
+	
+	// if the element has a parent, it is in the container, if not it needs to be added
+	if (this.isOnHomepage()) {
 		return;
 	}
-	else {
+
+	document.body.style.height = '';
+	document.body.style.width = '';
 
 
-		document.body.style.height = '';
-		document.body.style.width = '';
+	render.clearContainer();
+	this.masterContainer.appendChild(this.homepageElement);
 
-
-		render.clearContainer();
-		this.masterContainer.appendChild(this.homepageElement);
-	}
 }
 
 
@@ -56,3 +66,15 @@ var instance = new Homepage();
 
 Homepage.prototype.Homepage=Homepage;
 window.homepage = instance;
+
+
+//google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+  ga('create', 'UA-67419497-1', 'auto');
+  ga('send', 'pageview');
+  
+  
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 //z index list:
 
