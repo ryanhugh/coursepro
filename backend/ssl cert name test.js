e34b23d9b3619c426143963e269554ec295fd3a7
rysquash <rysquash@gmail.com>
2016-03-06 23:10:07 -0800

    clean up todo.txt some more

--- backend/ssl cert name test.js
+++ backend/ssl cert name test.js
@@ -4,12 +4,26 @@ var URI = require('urijs')
 
 // https.request('https://:9000',function (res) {
 
+// var options = {
+//   hostname: 'bappas2.gram.edu',
+//   port: 9000,
+//   path: '/pls/gram/bwckschd.p_disp_dyn_sched',
+//   method: 'GET'
+// };
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var options = {
-  hostname: 'bappas2.gram.edu',
-  port: 9000,
-  path: '/pls/gram/bwckschd.p_disp_dyn_sched',
+  hostname: 'localhost',
+  port: 8888,
+  path: '/',
+   headers: {'user-agent': 'Mozilla/5.0'},
   method: 'GET'
 };
+
+
+//this works when this is running
+ // ssh -L 8888:coursepro.io:443 app
+
+
 
 var req = https.request(options, function(res) {
   console.log('statusCode: ', res.statusCode);
@@ -20,6 +34,20 @@ var req = https.request(options, function(res) {
   });
 });
 req.end();
+
+
+//random notes that were in todo.txt
+// BASEPARSER.JS
+// https://bappas2.gram.edu:9000/pls/gram/bwckctlg.p_disp_dyn_ctlg
+// nodejs cant access a https over non standard port???
+// effcts: needle, https, requests, xmlhttprequest
+// works in python and in the browser (chrome)
+// use eff.org to test
+// update: could access localhost on port 8443 which forwarded from eff.org 443 (so non port 443 works, maybe something wierd with this site+nodejs?) (double check this on remote server)
+
+
+
+
 
 
 // return
