2e782fa35e9538bd6af0375f257796ae043e0fa5
rysquash <rysquash@gmail.com>
2016-03-07 21:55:36 -0800

    more research into 443 nodejs problems, it appears it is just the one site

--- backend/ssl cert name test.js
+++ backend/ssl cert name test.js
@@ -12,26 +12,68 @@ var URI = require('urijs')
 // };
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 var options = {
-  hostname: 'localhost',
-  port: 8888,
-  path: '/',
-   headers: {'user-agent': 'Mozilla/5.0'},
-  method: 'GET'
+	hostname: 'banssbprod.xavier.edu',
+	// hostname: 'localhost',
+	port: 8099,
+	path: '/PROD/bwckschd.p_disp_dyn_sched',
+	headers: {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+		'Host': 'bappas2.gram.edu:9000',
+		'Cache-Control':'max-age=0',
+		'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		'Upgrade-Insecure-Requests':'1',
+		'DNT':'1',
+		'Accept-Language':'en-US,en;q=0.8'
+	},
+	method: 'GET'
 };
 
 
+// https://appprod.udayton.edu:9000/prod/bwckschd.p_disp_dyn_sched
+
+
 //this works when this is running
- // ssh -L 8888:coursepro.io:443 app
+// when point this script to https://localhost:8888
+// ssh -L 8888:coursepro.io:443 app
+
+// but this does not work
+// it also dosent work when pointing to the server directly
+// when point this script to https://localhost:443
+// ssh -L 443:bappas2.gram.edu:9000 app
 
 
 
-var req = https.request(options, function(res) {
-  console.log('statusCode: ', res.statusCode);
-  console.log('headers: ', res.headers);
+//this does work
+// when point this script to https://localhost:443
+// ssh -L 443:appprod.udayton.edu:9000 app
 
-  res.on('data', function(d) {
-    process.stdout.write(d);
-  });
+
+//this does work
+// when point this script to https://appprod.udayton.edu:9000
+// ssh -L 443:appprod.udayton.edu:9000 app
+
+
+
+// this also works
+// https://oxford.blinn.edu:9010/PROD/bwckschd.p_disp_dyn_sched
+
+
+//works
+// https://ssbprod.rcgc.edu:9000/prod_ssb/bwckschd.p_disp_dyn_sched
+
+//works
+// https://banssbprod.xavier.edu:8099/PROD/bwckschd.p_disp_dyn_sched
+
+
+// which means it has be something with the servers???
+
+var req = https.request(options, function (res) {
+	console.log('statusCode: ', res.statusCode);
+	console.log('headers: ', res.headers);
+
+	res.on('data', function (d) {
+		process.stdout.write(d);
+	});
 });
 req.end();
 
