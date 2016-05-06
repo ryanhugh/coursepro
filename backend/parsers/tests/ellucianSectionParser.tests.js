9881f634b14bd535d377c4b293b28f9f3d38629c
rysquash <rysquash@gmail.com>
2016-05-05 21:18:35 -0700

    cleaned up the test jasmine file

--- backend/parsers/tests/ellucianSectionParser.tests.js
+++ backend/parsers/tests/ellucianSectionParser.tests.js
@@ -1,4 +1,4 @@
-// var ellucianSectionParser = require('../ellucianSectionParser')
+var ellucianSectionParser = require('../ellucianSectionParser')
 
 
 // it should either be 1. hidden, or have a times[0][0] that has > 0 length
@@ -8,7 +8,7 @@ describe('EllucianSectionParser', function () {
 		// return;
 		it('exists', function (done) {
 			
-			// expect(ellucianSectionParser).not.toBe(null);
+			expect(ellucianSectionParser).not.toBe(null);
 			console.log("one running")
 			setTimeout(function(){
 			console.log("one done")
@@ -23,64 +23,14 @@ describe('EllucianSectionParser', function () {
 		
 		it('exists22', function () {
 			
-			// expect(ellucianSectionParser).not.toBe(null);
+			expect(ellucianSectionParser).not.toBe(null);
 				console.log('2 running')
 			expect(1).toBe(1);
 				console.log('2 fail')
 			
 			
-			// setTimeout(function(){
-				// done()
-				
-			// },10)
-
 		});
 		
-				
-		  //afterEach(function() {
-		  	// console.log(jsApiReporter.specResults(0,20),'YOOO')
-		  	
-		    // {   var failed = jsApiReporter.specResults(this.index -1, 1)[0].failedExpectations;
-		    // if (this.index > 0)
-		    //     console.log('failed: ', failed);
-		    //     if (failed.length > 0)
-		    //     {
-		    //         console.log('After: ', this, failed[0].message);
-		    //         alert('ha');
-		    //     }
-		    // }
-		  //});
 		  
 	});
 });
-
-// describe('EllucianSectionParser', function () {
-
-//  var player;
-//   var song;
-//   var index = 0;
-
-//   beforeEach(function() {
-//     // player = new Player();
-//     // song = new Song();
-//     this.index = index++;
-//   });
-
-//   afterEach(function() {
-//     if (this.index > 0)
-//     {   var failed = jsApiReporter.specResults(this.index -1, 1)[0].failedExpectations;
-//         console.log('failed: ', failed);
-//         if (failed.length > 0)
-//         {
-//             console.log('After: ', this, failed[0].message);
-//             alert('ha');
-//         }
-//     }
-//   });
-//   it("should not fail", function()
-//   { 
-  	
-//   	console.log("FDSLJFLJSDL")
-//   	expect(1).toEqual(1);
-//   });
-//   });
