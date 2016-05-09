ffb1a325ea35a594122dda8a149209bb6c44f693
rysquash <rysquash@gmail.com>
2016-05-08 17:00:05 -0700

    added collision, going to remove it

--- frontend/static/test2.js
+++ frontend/static/test2.js
@@ -1,6 +1,6 @@
  var width = window.innerWidth;
  var height = 2000;
- var nodeWidth = 125;
+ var nodeWidth = 164;
  var nodeHeight = 50;
 
  var fill = d3.scale.category10();
@@ -22,19 +22,20 @@
  // 	return force.alpha(alpha);
  // };
 
-var container;
-function zoomed() {
-  container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
-}
+ var container;
 
-var zoom = d3.behavior.zoom()
-    .scaleExtent([1, 10])
-    .on("zoom", zoomed);
+ function zoomed() {
+ 	container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+ }
+
+ var zoom = d3.behavior.zoom()
+ 	.scaleExtent([1, 10])
+ 	.on("zoom", zoomed);
 
  var svg = d3.select("body").append("svg")
  	.attr("width", width)
  	.attr("height", height)
- 	  .call(zoom);
+ 	.call(zoom);
 
  // var drag = d3.behavior.drag()
  // 	.on('drag', function () {
@@ -68,18 +69,23 @@ var zoom = d3.behavior.zoom()
  		// force.resume() // were valid in "dragstart"
  		// d.fixed = true
  		// tick()
- 			dragInitiated = false
+ 		dragInitiated = false
  	}) // terminate drag gesture
 
  d3.json("thing2.json", function (error, graph) {
- 	graph.nodes.forEach(function(node){
- 		node.x = node.cx = 0
- 		node.y = node.cy = node.level * 200+50
- 		// node.fixed = true
+ 	graph.nodes.forEach(function (node) {
+ 		node.x = node.cx = Math.random() * 100 + 200
+ 		node.y = node.cy = node.level * 200 + 50
+ 		node.x2 = function () {
+ 			return node.x + nodeWidth
+ 		}
+ 		node.y2 = function () {
+ 			return node.y + nodeHeight
+ 		}
  	})
 
 
- container = svg.append("g");
+ 	container = svg.append("g");
 
  	var link = container.selectAll(".link")
  		.data(graph.links)
@@ -121,7 +127,7 @@ var zoom = d3.behavior.zoom()
  	// })
 
 
- 	node.html('<foreignObject width="164" height="50" style="transform: translate(-50%,-50%);"><div class="panel panel-primary"> <div class="panel-heading"> <h3 class="panel-title">Panel title</h3> </div> <div class="panel-body"> Panel content </div> </div></foreignObject>')
+ 	node.html('<foreignObject width="'+nodeWidth+'" height="'+nodeHeight+'" style="transform: translate(-50%,-50%);"><div class="panel panel-primary"> <div class="panel-heading"> <h3 class="panel-title">Panel title</h3> </div> <div class="panel-body"> Panel content </div> </div></foreignObject>')
 
 
  	for (var i = 0; i < node[0].length; i++) {
@@ -142,11 +148,20 @@ var zoom = d3.behavior.zoom()
  	// }, 6000);
 
  	function tick(e) {
- 		var k = e.alpha * multiplyer;
+ 		if (multiplyer === 15) {
+ 			var q = d3.geom.quadtree(graph.nodes),
+ 				i = 0,
+ 				n = graph.nodes.length;
+ 			while (++i < n) {
+ 				q.visit(collide(graph.nodes[i]));
+ 			}
+
+ 		}
+
  		graph.nodes.forEach(function (o, i) {
 
  			//possible to get the staticly set width and height here, node[0][o.index].lastChild.width.value
- 			o.y += ((o.level * 200 + 50) - o.y) * k;
+ 			o.y += ((o.level * 200 + 50) - o.y) * e.alpha * multiplyer;
  			// if (o.x < 0) {}
  		});
 
@@ -203,6 +218,13 @@ var zoom = d3.behavior.zoom()
  		console.log('success??');
  	}
 
+ 	setTimeout(function () {
+ 		force.start();
+
+ 		multiplyer = 15;
+
+ 	}.bind(this), 3000)
+
  });
  // svg.style("opacity", 1e-6)
  // .transition()
@@ -225,3 +247,61 @@ var zoom = d3.behavior.zoom()
  // // });
  // force.resume();
  // }
+
+
+ overlap = function (a, b) {
+ 	var _ref, _ref1, _ref2, _ref3;
+ 	return ((a.x < (_ref = b.x) && _ref < a.x2()) && (a.y < (_ref1 = b.y) && _ref1 < a.y2())) || ((a.x < (_ref2 = b.x2()) && _ref2 < a.x2()) && (a.y < (_ref3 = b.y2()) && _ref3 < a.y2()));
+ };
+
+ collide = function (node) {
+ 	var nx1, nx2, ny1, ny2, padding;
+ 	padding = 32;
+ 	nx1 = node.x - padding;
+ 	nx2 = node.x2() + padding;
+ 	ny1 = node.y - padding;
+ 	ny2 = node.y2() + padding;
+ 	return function (quad, x1, y1, x2, y2) {
+ 		var dy;
+ 		if (quad.point && (quad.point !== node)) {
+ 			if (overlap(node, quad.point)) {
+ 				// dy = Math.min(node.y2() - quad.point.y, quad.point.y2() - node.y) / 4;
+ 				// node.y -= dy;
+ 				// quad.point.y += dy;
+ 				dx = Math.min(node.x2() - quad.point.x, quad.point.x2() - node.x) / 3;
+ 				node.x -= dx;
+ 				quad.point.x += dx;
+ 			}
+ 		}
+ 		return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+ 	};
+ };
+
+ // function overlap(a, b) {
+ // 	return false;
+ // 	return (a.x < b.x < a.x2() && a.y < b.y < a.y2()) ||
+ // 		(a.x < b.x2() < a.x2() && a.y < b.y2() < a.y2())
+ // }
+
+ // function collide(node) {
+ // 	var nx1, nx2, ny1, ny2, padding;
+ // 	padding = 32;
+ // 	nx1 = node.x - padding;
+ // 	nx2 = node.x2() + padding;
+ // 	ny1 = node.y - padding;
+ // 	ny2 = node.y2() + padding;
+ // 	return function (quad, x1, y1, x2, y2) {
+ // 		var dx, dy;
+ // 		if (quad.point && (quad.point !== node)) {
+ // 			if (overlap(node, quad.point)) {
+ // 				dx = Math.min(node.x2() - quad.point.x, quad.point.x2() - node.x) / 2;
+ // 				node.x -= dx;
+ // 				quad.point.x += dx;
+ // 				dy = Math.min(node.y2() - quad.point.y, quad.point.y2() - node.y) / 2;
+ // 				node.y -= dy;
+ // 				quad.point.y += dy;
+ // 			}
+ // 		}
+ // 		return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+ // 	};
+ // };
