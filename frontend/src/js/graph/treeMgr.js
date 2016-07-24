f1aa879398a6e4b28767fe8b812ef83ec411ca49
Ryan <rysquash+bitbucket@gmail.com>
2016-07-24 16:54:45 -0400

    "making the svg not full screen"

--- frontend/src/js/graph/treeMgr.js
+++ frontend/src/js/graph/treeMgr.js
@@ -735,7 +735,7 @@ TreeMgr.prototype.defaultTo = function (tree, type) {
 };
 
 TreeMgr.prototype.getYGuessFromDepth = function (depth) {
-	return depth * 250 + 50;
+	return depth * 250 + 250;
 };
 
 // guess node positions attempts to estimate the output position of the d3 graph
