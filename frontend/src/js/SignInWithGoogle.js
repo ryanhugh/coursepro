7b8b4f31819b4cf03dd2cce413b39e4c6f909bf6
rysquash <rysquash@gmail.com>
2016-07-31 12:48:07 -0700

    \'\'

--- frontend/src/js/SignInWithGoogle.js
+++ frontend/src/js/SignInWithGoogle.js
@@ -5,40 +5,30 @@ var BaseDirective = require('./BaseDirective')
 
 var user = require('./user')
 
+
 function SignInWithGoogle() {
-
-	function SignInWithGoogleInner() {
-		this.scope = null;
-	}
-
-	SignInWithGoogleInner.scope = true;
-
-
-	SignInWithGoogleInner.prototype.link = function (scope, element, attrs) {
-		this.scope = scope;
-
-		var id = Math.random() + '' + Math.random()
-
-		element[0].id = id;
-
-		gapi.signin2.render(id, {
-			onsuccess: function (googleUser) {
-				user.signedInWithGoogle(null,googleUser.getAuthResponse().id_token)
-			}.bind(this),
-			onfailure: function (err) {
-				elog('Error signing in with google',err)
-				user.signedInWithGoogle(err)
-			}.bind(this)
-		});
-	}
-	var instance = new SignInWithGoogleInner();
-	instance.link = instance.link.bind(instance)
-
-	return instance;
 }
 
 SignInWithGoogle.fnName = 'SignInWithGoogle'
 
+SignInWithGoogle.prototype.link = function (scope, element, attrs) {
+
+	var id = Math.random() + '' + Math.random()
+
+	element[0].id = id;
+
+	gapi.signin2.render(id, {
+		onsuccess: function (googleUser) {
+			user.signedInWithGoogle(null, googleUser.getAuthResponse().id_token)
+		}.bind(this),
+		onfailure: function (err) {
+			elog('Error signing in with google', err)
+			user.signedInWithGoogle(err)
+		}.bind(this)
+	});
+}
+
+
 SignInWithGoogle.prototype.SignInWithGoogle = SignInWithGoogle;
 module.exports = SignInWithGoogle;
-directiveMgr.addLink(SignInWithGoogle)
+directiveMgr.addRawDirective(SignInWithGoogle)
