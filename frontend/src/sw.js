842ef16afc96f9dde01c055506ab86ca0913f9ac
rysquash <rysquash@gmail.com>
2016-08-06 19:43:51 -0700

    \'\'

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,32 +1,18 @@
-/*
- Copyright 2014 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
+'use strict';
+
+
+// More info about service workers:
+// https://serviceworke.rs/
+// This was created from this: https://googlechrome.github.io/samples/service-worker/read-through-caching/
+// and many other places on the internet
+
+
 
 // macros.DEVELOPMENT and macros.PRODUCTION are swapped with true and false by gulp-replace
+// this is just here so it dosen't crash if this isn't ran through gulp-replace
 var macros = {};
 
-// While overkill for this specific sample in which there is only one cache,
-// this is one best practice that can be followed in general to keep track of
-// multiple caches used by a given service worker, and keep them all versioned.
-// It maps a shorthand identifier for a cache to a specific, versioned cache name.
 
-// Note that since global state is discarded in between service worker restarts, these
-// variables will be reinitialized each time the service worker handles an event, and you
-// should not attempt to change their values inside an event handler. (Treat them as constants.)
-
-// If at any point you want to force pages that use this service worker to start using a fresh
-// cache, then increment the CACHE_VERSION value. It will kick off the service worker update
-// flow and the old cache(s) will be purged as part of the activate event handler when the
-// updated service worker is activated.
 var CACHE_VERSION = 1;
 var CURRENT_CACHES = {
   'read-through': 'read-through-cache-v' + CACHE_VERSION
@@ -48,11 +34,11 @@ this.addEventListener('activate', function (event) {
     caches.keys().then(function (cacheNames) {
       return Promise.all(
         cacheNames.map(function (cacheName) {
-          return caches.delete(cacheName);
-          // if (expectedCacheNames.indexOf(cacheName) === -1) {
-          //   // If this cache name isn't present in the array of "expected" cache names, then delete it.
-          //   console.log('Deleting out of date cache:', cacheName);
-          // }
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            return caches.delete(cacheName);
+            console.log('Deleting out of date cache:', cacheName);
+          }
         })
       );
     })
