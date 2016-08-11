e352f25ee244a560811901dbe3c3a315a56f5dd8
rysquash <rysquash@gmail.com>
2016-08-11 18:59:21 -0400

    \'added new files from last commit\'

---
+++ frontend/src/js/data/tests/term.tests.js
@@ -0,0 +1,43 @@
+'use strict';
+var Term = require('../Term')
+var macros = require('../macros')
+var _ = require('lodash')
+
+describe('.compareTo', function () {
+	it('works', function () {
+
+		var terms = [
+			new Term({
+				host: 'neu.edu',
+				termId: '201630'
+			}),
+			new Term({
+				host: 'neu.edu',
+				termId: '201710'
+			})
+		];
+
+		terms.sort(function (a, b) {
+			return a.compareTo(b);
+		})
+
+		expect(terms[0].termId).toBe('201710');
+	});
+
+});
+
+describe('.download', function () {
+	it('works', function (done) {
+
+		var term = Term.create({
+			host: 'neu.edu',
+			termId: '201630'
+		})
+
+		term.download(function () {
+			expect(term.text).toBe('Spring 2016')
+			done()
+		}.bind(this));
+	});
+
+});
