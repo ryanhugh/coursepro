db0ec232146290e7327182969ae2fe2a6b411dc3
rysquash <rysquash@gmail.com>
2016-09-03 15:46:41 -0400

    limit neu to 800.

--- backend/pointer.js
+++ backend/pointer.js
@@ -122,7 +122,7 @@ var throtteling = {
 	'telaris.wlu.ca': queue(400),
 	'myswat.swarthmore.edu': queue(1000),
 	'bannerweb.upstate.edu': queue(200),
-	'wl11gp.neu.edu': queue(2000)
+	'wl11gp.neu.edu': queue(800)
 }
 
 var infinateQueue = queue();
