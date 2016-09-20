5efa86b241f4bacca355acdd71d892e75f6d7259
rysquash <rysquash@gmail.com>
2016-09-20 14:08:43 -0400

    neu queue back to 2000.

--- backend/pointer.js
+++ backend/pointer.js
@@ -174,7 +174,7 @@ var throtteling = {
 	'telaris.wlu.ca': queue(400),
 	'myswat.swarthmore.edu': queue(1000),
 	'bannerweb.upstate.edu': queue(200),
-	'wl11gp.neu.edu': queue(800)
+	'wl11gp.neu.edu': queue(2000)
 }
 
 var infinateQueue = queue();
