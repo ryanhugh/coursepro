c7ba33828a722f6abe90b80f4657b3056fd3cd72
Ryan Hughes <rysquash@gmail.com>
2017-01-02 00:19:51 -0500

    added the license header to the html files.

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 for i in $(find . | grep .*.html\$) # or whatever other pattern...
 do
 	echo $i
-  # if ! grep -q Copyright $i
-  # then
-  #   cat header.txt $i >$i.new && mv $i.new $i
-  # fi
+  if ! grep -q Copyright $i
+  then
+    cat header.txt $i >$i.new && mv $i.new $i
+  fi
 done
